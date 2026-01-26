23961de6784a8a20f158c07cdf1cd801dd9ed29a
lucasA49 <lucasaksu@gmail.com>
2026-01-26 18:47:20 +0100

    mise en place de tailwinds css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Pages publiques
+import Home from "./Pages/Home.jsx";
+
+
+const router = createBrowserRouter([
+  // -------------------------
+  // PAGES PUBLIQUES
+  // -------------------------
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
